7863d59aa54bb096e5eb401cb9dd1b9b32fe79bb
bumfo <bumfo@users.noreply.github.com>
2019-08-17 13:30:26 +0800

    Test action deploy

---
+++ src/deploy.sh
@@ -0,0 +1,42 @@
+#!/bin/sh
+
+REF="$1"
+if [ -z "${REF}" ]; then
+    echo Empty ref specified
+    exit 1
+fi
+
+if [ -z "$GITHUB_TOKEN" ]; then
+    echo Token invalid
+    exit 1
+fi
+
+REMOTE_REPO="https://x-access-token:${GITHUB_TOKEN}@github.com/${GITHUB_REPOSITORY}.git"
+
+cd "${GITHUB_WORKSPACE}" || exit 1
+git config user.name "${GITHUB_ACTOR}"
+git config user.email "${GITHUB_ACTOR}@users.noreply.github.com"
+
+if git diff --exit-code -s HEAD "${REMOTE_REPO}"/"${GITHUB_REF}" -- . ':!node_modules'; then
+    :
+else
+    echo HEAD outdated
+    exit 1
+fi
+
+npm i
+npm run build || exit 1
+npm run test || exit 1
+
+rm -rf node_modules
+npm i --production
+git add -f node_modules
+git add .
+
+if git commit -am "Deployed From Action ${GITHUB_ACTION}"; then
+    git push -f "${REMOTE_REPO}" HEAD:"${REF}"
+    exit "$?"
+else
+    echo Nothing to push
+    exit 0
+fi
--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,20 @@
 import * as core from '@actions/core';
+import * as exec from '@actions/exec';
+import * as io from '@actions/io';
+import path from 'path';
 
 async function run() {
   try {
-    const myInput = core.getInput('myInput');
-    core.debug(`Hello ${myInput}`);
+    var cwd = path.resolve(__dirname, '..')
+
+    const ref = core.getInput('ref');
+    console.log(`Deploying to ${ref}`);
+
+    const options = {
+      cwd: cwd,
+    };
+
+    await exec.exec(`${await io.which('bash', true)}`, ['src/deploy.sh', ref], options);
   } catch (error) {
     core.setFailed(error.message);
   }
